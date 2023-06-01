124ff1a13e414e69b36b36c86e6de1090128cfd9
AdaiasMartins <110681069+AdaiasMartins@users.noreply.github.com>
2023-06-01 10:09:51 -0300

    Update Produto.java
    Atualizando a classe produto

--- Mercado/src/main/java/org/example/Produto.java
+++ Mercado/src/main/java/org/example/Produto.java
@@ -8,18 +8,35 @@ public class Produto {
 
     private String nome;
 
-    public Produto(double preco, String nome) throws ProdutoInvalidoException {
+    private int desconto = 0;
+
+    public Produto(double preco, String nome) throws ItemInvalidoException {
         validaProduto(preco, nome);
         this.preco = preco;
         this.nome = nome;
     }
 
-    public void validaProduto(double preco, String nome) throws ProdutoInvalidoException {
+    public void validaProduto(double preco, String nome) throws ItemInvalidoException {
         if (preco <= 0){
-            throw new ProdutoInvalidoException("O valor do produto não pode ser menor ou igual a zero");
+            throw new ItemInvalidoException("O valor do produto não pode ser menor ou igual a zero");
         }
         if (nome == null){
-            throw new ProdutoInvalidoException("O nome do produto não pode estar vazio");
+            throw new ItemInvalidoException("O nome do produto não pode estar vazio");
+        }
+    }
+
+    public void calculaDesconto(int desconto) throws ItemInvalidoException{
+        validaDesconto(desconto);
+        this.desconto = desconto;
+        this.preco = preco - (preco * (desconto/100));
+    }
+
+    public void validaDesconto(int desconto) throws ItemInvalidoException {
+        if(desconto < 0){
+            throw new ItemInvalidoException("O desconto não pode ser menor que zero");
+        }
+        if(desconto > 100){
+            throw new ItemInvalidoException("O desconto do item não pode ultrapassar cem porcento");
         }
     }
 
@@ -31,5 +48,23 @@ public class Produto {
         this.nome = nome;
     }
 
+    public void setDesconto(int desconto){
+        this.desconto = desconto;
+    }
 
+    public int getDesconto() {
+        return desconto;
+    }
+
+    public double getPreco(){
+        return preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String toString(){
+        return "Produto do tipo " + nome + " com o desconto de: " + desconto + "%" + " totalizando: R$" + preco;
+    }
 }
